ed7f56263b6747a894ba49f468178e236060f6c3
roelberry <rochelleberry731@gmail.com>
2025-08-20 02:29:24 +0000

    ok

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -4,6 +4,12 @@ import { seo } from 'wix-seo';
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 $w.onReady(function () {
+    seo.setSeoTags({
+        title: 'masterPage',
+        description: 'Explore the masterPage page at the Kokomo Art Association.'
+    });
+
+
     // Write your JavaScript here
 
     // Decorative background image
