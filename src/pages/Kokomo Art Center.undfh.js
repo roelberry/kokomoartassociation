84f9e02bf466558a16bb42471714877675e2b97e
roelberry <rochelleberry731@gmail.com>
2025-08-19 20:45:52 -0400

    Add JSON-LD structured data to key pages

--- src/pages/Kokomo Art Center.undfh.js
+++ src/pages/Kokomo Art Center.undfh.js
@@ -1,7 +1,16 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+import { seo } from 'wix-seo';
 
 $w.onReady(function () {
+    const schemaObject = {
+        '@context': 'https://schema.org',
+        '@type': 'Organization',
+        name: 'Kokomo Art Center',
+        url: 'https://www.kokomoart.org'
+    };
+    seo.setCustomMetaTags([{ tagName: 'script', attributes: { type: 'application/ld+json' }, content: JSON.stringify(schemaObject) }]);
+
     // Write your JavaScript here
 
     // To select an element by ID use: $w('#elementID')
